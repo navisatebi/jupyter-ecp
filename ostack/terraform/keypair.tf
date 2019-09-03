840160f8ff8bf5509a0bb632b5a02980e373123c
Navis <navis@ebi.ac.uk>
2019-09-03 10:52:41 +0100

    Update keypair.tf

--- ostack/terraform/keypair.tf
+++ ostack/terraform/keypair.tf
@@ -1,4 +1,4 @@
 resource "openstack_compute_keypair_v2" "keypair" {
   name       = "${var.name}-keypair"
-  public_key = "${var.ssh_key}"
+  public_key = "${replace("${file("${var.public_key_path}")}", "\n" , "")}"
 }
