eb5e9e0270c0b94aecbec07bd680ea170c37680d
Navis <navis@ebi.ac.uk>
2019-10-11 16:17:02 +0100

    Update deploy.sh

--- ostack/deploy.sh
+++ ostack/deploy.sh
@@ -51,8 +51,8 @@ ansible-galaxy install -r requirements.yml
 export ANSIBLE_REMOTE_USER="${TF_VAR_remote_user:-ubuntu}"
 echo "export ANSIBLE_REMOTE_USER=${ANSIBLE_REMOTE_USER}"
 
-export name="${name}"
-echo "export name=${name}"
+export TF_VAR_name="${TF_VAR_name}"
+echo "export TF_VAR_name=${TF_VAR_name}"
 
 # Launch Ansible playbook
 echo -e "\n\t${CYAN}Launch Ansible playbook${NC}\n"
